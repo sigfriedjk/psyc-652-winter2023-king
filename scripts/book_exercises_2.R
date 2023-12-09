55ce18c91ed71c6fea93d59c2b69b02bc8ba9f66
Jonathan King <jcking@umd.edu>
2023-12-09 20:06:07 +0000

    Adds lesson 16; starts 17

--- scripts/book_exercises_2.R
+++ scripts/book_exercises_2.R
@@ -3,3 +3,15 @@ library(dplyr)
 library(ggplot2)
 writeLines("\n\nStarting Book Exercises 2")
 
+lesson19e1 <- "data_sets/Green_Salkind_SPSS_Data_Sets/Lesson_16_Data_File_1.sav"
+writeLines("Starting Read")
+data <- read_sav(lesson19e1)
+writeLines("Read Complete")
+
+data
+
+writeLines("Creating Line Graph")
+jpeg(filename="/tmp/book_exercises_2/lesson-16A-line.jpg")
+plot(data,type = "o", main ="Score by Grade")
+dev.off()
+
--- scripts/main.sh
+++ scripts/main.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 Rscript lesson19.R > /tmp/lesson19.log
 Rscript homework1.R > /tmp/homework1/homework1.log
+Rscript book_exercises_2.R # > /tmp/book_exercises_2/book_exercises_2.log
